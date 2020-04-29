6deb84fc54eb69445be27aeddca4b87c226949e4
kiana <kiana.hobbs@gmail.com>
2020-04-29 02:21:40 -0500

    Random Coolness

--- 10print.js
+++ 10print.js
@@ -3,18 +3,50 @@
 */
 
 const w = process.stdout.columns
+var wreduced = w
 
 function draw () {
-  setTimeout(draw, 1000)
-  let output = ''
-  for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
+  setTimeout(draw, 100)
+  let output = '\033[0;41m'
+  var change = Math.round(w * Math.random())
+  if( wreduced < w * Math.random()){
+    for (let i = wreduced; i < change; i++) {
+      var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
+      if (Math.random() > 0.8) {
+        output += `${color}╝`
+      } else if (Math.random() > 0.6) {
+        output += `${color}╚`
+      }else if (Math.random() > 0.4) {
+        output += `${color}╔`
+      }else if (Math.random() > 0.2) {
+        output += `${color}╗`
+      }else {
+        output += `${color}╬`
+      }
     }
+      wreduced = wreduced + 1
+
   }
+  else{
+    for (let i = wreduced; i > 0; i--) {
+      var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
+      if (Math.random() > 0.8) {
+        output += `${color}╝`
+      } else if (Math.random() > 0.6) {
+        output += `${color}╚`
+      }else if (Math.random() > 0.4) {
+        output += `${color}╔`
+      }else if (Math.random() > 0.2) {
+        output += `${color}╗`
+      }else {
+        output += `${color}╬`
+      }
+    }
+    wreduced = wreduced - 1
+  }
+
   console.log(output)
+
 }
 
 draw()
