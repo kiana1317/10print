30d9fa174568db2e7b43d2876cc0a45413a03ee8
kiana <kiana.hobbs@gmail.com>
2020-04-30 22:15:30 -0500

    Not the most pretty code, but has cool animations

--- 10print.js
+++ 10print.js
@@ -3,50 +3,67 @@
 */
 
 const w = process.stdout.columns
-var wreduced = w
+var w1 = Math.round(w * Math.random())
+var w2 = Math.round(w * Math.random())
+var max = Math.max(w1,w2)
+var min = Math.min(w1,w2)
+var positive = true
 
 function draw () {
   setTimeout(draw, 100)
-  let output = '\033[0;41m'
-  var change = Math.round(w * Math.random())
-  if( wreduced < w * Math.random()){
-    for (let i = wreduced; i < change; i++) {
-      var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
-      if (Math.random() > 0.8) {
-        output += `${color}╝`
-      } else if (Math.random() > 0.6) {
-        output += `${color}╚`
-      }else if (Math.random() > 0.4) {
-        output += `${color}╔`
-      }else if (Math.random() > 0.2) {
-        output += `${color}╗`
-      }else {
-        output += `${color}╬`
+  let output = ''
+  if (min === max || max >= w || min <= 0) {
+    positive = !positive
+    if(positive){
+      min = Math.round(w * Math.random())
+      while(min >= max){
+        min = Math.round(w * Math.random())
       }
     }
-      wreduced = wreduced + 1
-
+    else{
+      max = Math.round(w * Math.random())
+      while(max <= min){
+        max = Math.round(w * Math.random())
+      }
+    }
+  }
+  if(positive){
+    for (let i = 0; i < max; i++) {
+        var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
+        if (Math.random() > 0.8) {
+            output += `${color}╝`
+        } else if (Math.random() > 0.6) {
+            output += `${color}╚`
+        }else if (Math.random() > 0.4) {
+            output += `${color}╔`
+        }else if (Math.random() > 0.2) {
+            output += `${color}╗`
+        }else {
+            output += `${color}╬`
+        }
+    }
+    console.log(output)
+    max = max - 1
   }
   else{
-    for (let i = wreduced; i > 0; i--) {
-      var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
-      if (Math.random() > 0.8) {
-        output += `${color}╝`
-      } else if (Math.random() > 0.6) {
-        output += `${color}╚`
-      }else if (Math.random() > 0.4) {
-        output += `${color}╔`
-      }else if (Math.random() > 0.2) {
-        output += `${color}╗`
-      }else {
-        output += `${color}╬`
-      }
+    for (let i = 0; i < min; i++) {
+        var color = (Math.round(i*Math.random()) % 2 === 0) ? '\033[0;35m' : '\033[0;36m'
+        if (Math.random() > 0.8) {
+            output += `${color}╝`
+        } else if (Math.random() > 0.6) {
+            output += `${color}╚`
+        }else if (Math.random() > 0.4) {
+            output += `${color}╔`
+        }else if (Math.random() > 0.2) {
+            output += `${color}╗`
+        }else {
+            output += `${color}╬`
+        }
     }
-    wreduced = wreduced - 1
+    console.log(output)
+    min = min +  1
   }
-
-  console.log(output)
-
 }
+
 
 draw()
